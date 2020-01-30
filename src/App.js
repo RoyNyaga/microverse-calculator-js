49881e4356843d820e56fdfd75dd9cfb9dcf3205
Roy Nyaga <nyagaandreroy@gmail.com>
2020-01-30 16:50:38 +0100

    deleted all unercessary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
 
-class App extends React.Compenent {
+import './App.css'
 
-  render(){
-    return(
-      <div className="app-wrapper">
-        <div className="jumbotron">
-          <h1 className="display-3">Hello, world!</h1>
-          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <hr className="m-y-md">
+class App extends React.Component {
+  render () {
+    return (
+      <div className='app-wrapper'>
+        <div className='jumbotron'>
+          <h1 className='display-3'>Hello, world!</h1>
+          <p className='lead'>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className='m-y-md' />
           <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <p className="lead">
-            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          <p className='lead'>
+            <a className='btn btn-primary btn-lg' href='/' role='button'>Learn more</a>
           </p>
         </div>
       </div>
-      );
+    )
   }
 }
-export default App;
+export default App

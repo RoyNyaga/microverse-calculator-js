01a7071177ed6c708326497bf9076d3f4b9d843f
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-07 18:08:06 +0100

    added Big data module and operator function in operator.js file

---
+++ src/logic/operator.js
@@ -0,0 +1,26 @@
+const Big = require('big.js')
+
+const operator = (numberOne, numberTwo, operation) => {
+  const bigNumberOne = Big(numberOne)
+  const bigNumberTwo = Big(numberTwo)
+}
+
+export default function operate (numberOne, numberTwo, operation) {
+  const number1 = Big(numberOne)
+  const number2 = Big(numberTwo)
+  switch (operation) {
+    case '+':
+      return number1.plus(number2).toString()
+    case '-':
+      return number1.minus(number2).toString()
+    case 'x':
+      return number1.times(number2).toString()
+    case '/':
+      if (numberTwo === '0') { alert('Division by Zero unauthorized') } else { return number1.div(number2).toString() }
+      break
+    case '%':
+      return number1.mod(number2).toString()
+    default:
+      alert(`operation '${operation}' not available `)
+  }
+}
